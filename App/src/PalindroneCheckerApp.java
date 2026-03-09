ca66c3c172b59a715531571eba3a8f0a62a50bf5
Farjan Alam <115532270+SyntaxSaviour@users.noreply.github.com>
2026-03-10 00:17:48 +0530

    Farjan Alam Added UC3-ReverseStringPalindrome

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -14,7 +14,28 @@ public class PalindroneCheckerApp {
         System.out.println("======================================");
 
         System.out.println("System initialized successfully.");
+        /*
+        // UC1 – Welcome message
+        System.out.println("Welcome to Palindrome Checker App");
+        System.out.println("Version 2.0");
 
+        // UC2 – Hardcoded palindrome check
+        String word = "madam";
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("System initialized successfully.");
+        if (isPalindrome)
+            System.out.println(word + " is a Palindrome");
+        else
+            System.out.println(word + " is NOT a Palindrome");
+        */
         // UC3 – Reverse string palindrome check
         String input = "madam";
         String reversed = "";
